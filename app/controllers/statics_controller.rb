be81eb5954c91c14767597b8d95094fec7612212
Ronald <dada5714@yahoo.com>
2014-10-08 16:03:23 -0700

    Fixed Syntax

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,10 +1,4 @@
 class StaticController < ApplicationController
   def update
-    @user = current_user
-    @user.background(params[:user][:background])
-    if @user.save
-      redirect_to root_url
-    else
-      redirect_to root_url, notice: "Hey Buddy, come on!"
   end
 end
