b8b061892faa47c3f0836b393126923cf9d36152
Philip Nguyen <supertaru@gmail.com>
2014-10-09 13:20:26 -0700

    will_paginate done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -15,11 +15,6 @@ class Bookmark < ActiveRecord::Base
 
   def self.search(query)
     if query.present?
-      #   rank = <<-RANK
-      #     ts_rank(to_tsvector(info), plainto_tsquery(#{sanitize(query)})) +
-      #     ts_rank(to_tsvector(name), plainto_tsquery(#{sanitize(query)}))
-      #   RANK
-      # where("info @@ :q or name @@ :q", q: query).order("#{rank} desc")
       super_search query
     else
       order('bookmarks.created_at DESC').all
