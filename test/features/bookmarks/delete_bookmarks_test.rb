33fde14c564c20452058dc9dc36b8a4750cf170d
Philip Nguyen <supertaru@gmail.com>
2014-10-09 17:15:06 -0700

    tests

---
+++ test/features/bookmarks/delete_bookmarks_test.rb
@@ -0,0 +1,12 @@
+# require 'test_helper'
+
+# describe 'Deleting bookmarks from private page' do
+#   it 'should let users delete their bookmarks' do
+#     sign_in
+#     previous_link_count = page.all
+
+#     page.all('x')[0].click
+
+#     page.all('.block_link').count.must_equal
+#   end
+# end
