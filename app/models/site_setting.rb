b86e09be603b5ef8c34079ae334feef468017757
Philip Nguyen <supertaru@gmail.com>
2014-11-23 13:43:30 -0800

    Change all mentions of omarker/link.ly to Kiniori

---
+++ app/models/site_setting.rb
@@ -0,0 +1,7 @@
+class SiteSetting
+  # All site default attributes go here. Example #title, #subtitle, etc...
+
+  def self.title
+    'Kiniori'
+  end
+end
--- test/features/navigation/navigation_test.rb
+++ test/features/navigation/navigation_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 describe "User navigating web page" do
   it "should be on home screen" do
     visit root_path
-    page.text.must_include "Link.ly"
+    page.text.must_include SiteSetting.title
   end
 end
