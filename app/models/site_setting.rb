120c59a0d0da7866a8f37f1166e645d25130e223
Philip Nguyen <supertaru@gmail.com>
2014-11-23 13:59:31 -0800

    Add comments to SiteSetting class

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -1,5 +1,6 @@
 class SiteSetting
-  # All site default attributes go here. Example #title, #subtitle, etc...
+  # All site default attributes go here. #title, #subtitle, #slogan, etc...
+  # Simply reference SiteSetting anywhere in the app to retrieve attributes
 
   def self.title
     'Kiniori'
